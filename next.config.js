523e272463421547c652ee7795f703a9ff232eab
Kraipon Najaroon <kraipon.na10@gmail.com>
2023-03-06 17:16:46 +0700

    initial app

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextTranslate = require("next-translate-plugin");
+
+module.exports = nextTranslate();
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
