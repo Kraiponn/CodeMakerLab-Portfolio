8630aef49ac469adb4c740a8b14e8061e3527509
Kraipon Najaroon <kraipon.na10@gmail.com>
2023-03-12 04:32:36 +0700

    add contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -7,10 +7,7 @@ import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
 
 import { FaHandPointRight } from "react-icons/fa";
-import ProfileLogo from "../../../public/images/profile_.jpg";
-
-import { IProject } from "@/interfaces";
-import { portfolioProjects, portfolioProjectsTh } from "@/utils/data";
+import ContactLogo from "../../public/images/contact-img.svg";
 
 interface Props {}
 
@@ -26,9 +23,70 @@ export default function Contact({}: Props) {
   return (
     <div
       id="contact"
-      className="flex items-center justify-center w-full h-screen px-7"
+      className="w-full h-screen px-16 py-[54px] flex flex-col md:flex-row items-center"
     >
-      <h1 className="text-2xl font-bold">{t("contact.title")}</h1>
+      {/*-------    Contact Logo    -------*/}
+      <div className="relative hidden p-2 md:block">
+        <Image
+          src={ContactLogo}
+          alt="logo"
+          priority
+          quality={100}
+          sizes="100vw"
+          width={0}
+          height={0}
+          className=""
+        />
+      </div>
+
+      {/*-------    Contact Form    -------*/}
+      <div className="w-[100%] md:w-[70%] mt-7">
+        <h1 className="px-5 text-lg font-bold md:text-2xl lg:text-4xl">{`Get In Touch`}</h1>
+
+        {/* <form action="post"> */}
+        <div className="flex flex-col items-center justify-start w-full gap-3 mt-5 md:flex-row">
+          <input
+            type="text"
+            placeholder={`First Name`}
+            className="w-full px-5 py-3 border-2 rounded-xl"
+          />
+          <input
+            type="text"
+            placeholder={`Last Name`}
+            className="w-full px-5 py-3 border-2 rounded-xl"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-start w-full gap-3 mt-5 md:flex-row">
+          <input
+            type="email"
+            placeholder={`Email Address`}
+            className="w-full px-5 py-3 border-2 rounded-xl"
+          />
+          <input
+            type="text"
+            placeholder={`Phone No.`}
+            className="w-full px-5 py-3 border-2 rounded-xl"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-start w-full gap-3 mt-5 md:flex-row">
+          <textarea
+            name="message"
+            rows={5}
+            placeholder={`Message`}
+            className="w-full px-5 py-3 border-2 rounded-xl"
+          />
+        </div>
+
+        <div className="mt-7">
+          <button
+            type="button"
+            className="py-3 font-bold text-white border-none rounded-lg hover:-translate-y-1 hover:duration-200 hover:text-slate-500 bg-rose-500 hover:bg-rose-400 px-9"
+          >{`SEND`}</button>
+        </div>
+        {/* </form> */}
+      </div>
     </div>
   );
 }
